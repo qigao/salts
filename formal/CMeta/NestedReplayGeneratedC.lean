1abcdb92d48ce960e491a799df1ecae4ce6c47ac
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 13:40:01 +0800

    formal: snapshot nested replay C witness

---
+++ formal/CMeta/NestedReplayGeneratedC.lean
@@ -0,0 +1,6 @@
+namespace CMeta.NestedReplayGeneratedC
+def distinctCount : Nat := 6
+def depth2Count : Nat := 4
+def depth3Count : Nat := 8
+def depth4Count : Nat := 16
+end CMeta.NestedReplayGeneratedC
