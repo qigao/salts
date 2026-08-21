0f937e846cdebcad2614955122c178df75ee07f7
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 23:34:28 +0800

    test(formal): add calculus module isolation red

---
+++ formal/CMeta/ModuleMigrationConformance.lean
@@ -0,0 +1,12 @@
+import CMeta.Calculus
+
+#check CMeta.product
+#check CMeta.CoreExpr
+#check CMeta.CoreExpr.eval
+#check CMeta.ppRepeat
+#check CMeta.replay
+
+assert_not_exists CMeta.product_length
+assert_not_exists CMeta.CoreExpr.eval_length_eq_cardinality
+assert_not_exists CMeta.replay_zip
+#check_assertions
