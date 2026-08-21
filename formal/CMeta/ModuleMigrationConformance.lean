74eeb1ef9edc28302f854978c0eb4236d94a970e
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 23:41:40 +0800

    test(formal): add traits module isolation red

--- formal/CMeta/ModuleMigrationConformance.lean
+++ formal/CMeta/ModuleMigrationConformance.lean
@@ -1,4 +1,5 @@
 import CMeta.Calculus
+import CMeta.Traits
 
 #check CMeta.product
 #check CMeta.CoreExpr
@@ -9,3 +10,15 @@ import CMeta.Calculus
 assert_not_exists CMeta.product_length
 assert_not_exists CMeta.CoreExpr.eval_length_eq_cardinality
 assert_not_exists CMeta.replay_zip
+
+#check CMeta.CType
+#check CMeta.Signature
+#check CMeta.Traits
+#check CMeta.Traits.inferUnary
+#check CMeta.SignaturePolicy
+#check CMeta.policyAllows
+
+assert_not_exists CMeta.Traits.type_unique
+assert_not_exists CMeta.Traits.inferUnary_of_known
+assert_not_exists CMeta.Traits.inferUnary_unique
+assert_not_exists CMeta.policyAllows_iff
