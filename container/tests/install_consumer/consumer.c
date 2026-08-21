3b54290c2027c00f892359742c689020098f018a
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 00:28:48 +0800

    test(turbostl): require renamed public package contract

--- container/tests/install_consumer/consumer.c
+++ container/tests/install_consumer/consumer.c
@@ -1,4 +1,4 @@
-#include <turbo/container/typed.h>
+#include <turbo/stl/typed.h>
 
 Containers((Vec, InstalledInts, int));
 
@@ -6,9 +6,9 @@ int main(void) {
   InstalledInts values = {0};
   int out = 0;
 
-  if (InstalledInts_init(&values, 2u) != CONTAINER_OK) return 1;
-  if (InstalledInts_push(&values, 7) != CONTAINER_OK) return 2;
-  if (InstalledInts_pop(&values, &out) != CONTAINER_OK || out != 7) return 3;
+  if (InstalledInts_init(&values, 2u) != TURBO_STL_OK) return 1;
+  if (InstalledInts_push(&values, 7) != TURBO_STL_OK) return 2;
+  if (InstalledInts_pop(&values, &out) != TURBO_STL_OK || out != 7) return 3;
   InstalledInts_destroy(&values);
   return 0;
 }
