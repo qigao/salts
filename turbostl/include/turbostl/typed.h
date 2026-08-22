bdc8eb3cf5fe07bbe52f47606eb62ff29ff4122c
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 19:17:20 +0800

    refactor(turbostl): hide generated container types

--- turbostl/include/turbostl/typed.h
+++ turbostl/include/turbostl/typed.h
@@ -16,7 +16,6 @@
 #include <turbostl/multimap.h>
 #include <turbostl/btree.h>
 #include <turbostl/bplus_tree.h>
-#include <turbostl/meta.h>
 
 /* Self-describing declaration DSL. These declarations bind CMeta metadata but
  * perform no allocation and do not create generated user-visible C types. */
@@ -77,41 +76,5 @@
   bplus_tree_t name = { .key_type = CMETA_TYPEOF(K), \
                         .value_type = CMETA_TYPEOF(V) }
 #endif
-
-/* Legacy generated-kind registrations remain temporarily for repository
- * migration only. They are removed once every TurboSTL consumer uses the
- * self-describing handle model. */
-#define CMETA_GENERIC_KIND_Vec CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_Deque CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_List CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_Stack CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_Queue CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_Heap CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_Set CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_HashSet CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_HashMap CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_Map CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_MultiMap CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_BTree CMETA_GENERIC_PROBE()
-#define CMETA_GENERIC_KIND_BPlusTree CMETA_GENERIC_PROBE()
-
-#define CMETA_TYPED_Vec(name, type) TURBO_VEC_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_Deque(name, type) TURBO_DEQUE_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_List(name, type) TURBO_LIST_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_Stack(name, type) TURBO_STACK_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_Queue(name, type) TURBO_QUEUE_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_Heap(name, type) TURBO_HEAP_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_Set(name, type) TURBO_SET_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_HashSet(name, type) TURBO_HASH_SET_DEFINE(name, type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_HashMap(name, key_type, value_type) \
-  TURBO_HASH_MAP_DEFINE(name, key_type, value_type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_Map(name, key_type, value_type) \
-  TURBO_MAP_DEFINE(name, key_type, value_type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_MultiMap(name, key_type, value_type) \
-  TURBO_MULTI_MAP_DEFINE(name, key_type, value_type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_BTree(name, key_type, value_type) \
-  TURBO_BTREE_DEFINE(name, key_type, value_type) enum { name##_cmeta_typed = 1 }
-#define CMETA_TYPED_BPlusTree(name, key_type, value_type) \
-  TURBO_BPLUS_TREE_DEFINE(name, key_type, value_type) enum { name##_cmeta_typed = 1 }
 
 #endif /* TURBO_TYPED_H */
