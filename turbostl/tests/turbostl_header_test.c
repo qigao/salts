fbd1e7f91b3a5fcb9cb8cb57e3328e688dad9b94
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 18:43:53 +0800

    test(turbostl): require inferred sequence declarations

--- turbostl/tests/turbostl_header_test.c
+++ turbostl/tests/turbostl_header_test.c
@@ -16,4 +16,45 @@ suite("TurboSTL public header") {
         check_equal(vec_init(&vec, 2u), STL_OK);
         vec_destroy(&vec);
     }
+
+    it("infers deque stack queue and heap types from declarations") {
+        Deque(int, deque);
+        Stack(int, stack);
+        Queue(int, queue);
+        Heap(int, heap);
+        int input = 5;
+        int output = 0;
+
+        check_equal(deque_init(&deque, 2u), STL_OK);
+        check_equal(deque_push_back(&deque, &input), STL_OK);
+        check_equal(deque_pop_front(&deque, &output), STL_OK);
+        check_equal(output, 5);
+        deque_destroy(&deque);
+        check_equal(deque_init(&deque, 3u), STL_OK);
+        deque_destroy(&deque);
+
+        check_equal(stack_init(&stack, 2u), STL_OK);
+        check_equal(stack_push(&stack, &input), STL_OK);
+        check_equal(stack_pop(&stack, &output), STL_OK);
+        check_equal(output, 5);
+        stack_destroy(&stack);
+        check_equal(stack_init(&stack, 3u), STL_OK);
+        stack_destroy(&stack);
+
+        check_equal(queue_init(&queue, 2u), STL_OK);
+        check_equal(queue_push(&queue, &input), STL_OK);
+        check_equal(queue_pop(&queue, &output), STL_OK);
+        check_equal(output, 5);
+        queue_destroy(&queue);
+        check_equal(queue_init(&queue, 3u), STL_OK);
+        queue_destroy(&queue);
+
+        check_equal(heap_init(&heap, 2u), STL_OK);
+        check_equal(heap_push(&heap, &input), STL_OK);
+        check_equal(heap_pop(&heap, &output), STL_OK);
+        check_equal(output, 5);
+        heap_destroy(&heap);
+        check_equal(heap_init(&heap, 3u), STL_OK);
+        heap_destroy(&heap);
+    }
 }
