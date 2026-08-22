a9912b1474b18be03a1166061f383fffd0153b59
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 18:48:07 +0800

    test(turbostl): require inferred associative declarations

--- turbostl/tests/turbostl_header_test.c
+++ turbostl/tests/turbostl_header_test.c
@@ -57,4 +57,33 @@ suite("TurboSTL public header") {
         check_equal(heap_init(&heap, 3u), STL_OK);
         heap_destroy(&heap);
     }
+
+    it("infers set hash-set and hash-map types from declarations") {
+        Set(int, ordered);
+        HashSet(int, hashed);
+        HashMap(int, int, table);
+        int key = 4;
+        int value = 16;
+
+        check_equal(set_init(&ordered, 4u), STL_OK);
+        check_equal(set_add(&ordered, &key), STL_OK);
+        check_true(set_contains(&ordered, &key));
+        set_destroy(&ordered);
+        check_equal(set_init(&ordered, 8u), STL_OK);
+        set_destroy(&ordered);
+
+        check_equal(hash_set_init(&hashed, 4u), STL_OK);
+        check_equal(hash_set_add(&hashed, &key), STL_OK);
+        check_true(hash_set_contains(&hashed, &key));
+        hash_set_destroy(&hashed);
+        check_equal(hash_set_init(&hashed, 8u), STL_OK);
+        hash_set_destroy(&hashed);
+
+        check_equal(hash_map_init(&table, 4u), STL_OK);
+        check_equal(hash_map_put(&table, &key, &value), STL_OK);
+        check_equal(*(const int *)hash_map_get_const(&table, &key), 16);
+        hash_map_destroy(&table);
+        check_equal(hash_map_init(&table, 8u), STL_OK);
+        hash_map_destroy(&table);
+    }
 }
