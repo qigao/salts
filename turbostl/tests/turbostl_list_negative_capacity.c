43aa6082df5dbe5e62e1f635090d888e5b011e84
Qi Gao <qi.gao2012@gmail.com>
2026-08-22 10:26:24 +0800

    test(turbostl): remove forbidden-list API probe source

--- turbostl/tests/turbostl_list_negative_capacity.c
+++
@@ -1,9 +0,0 @@
-#include <turbostl/list.h>
-
-int main(void) {
-    turbo_stl_status (*reserve_fn)(turbo_list_t *, size_t) = turbo_list_reserve;
-    size_t (*capacity_fn)(const turbo_list_t *) = turbo_list_capacity;
-    void *(*at_fn)(turbo_list_t *, size_t) = turbo_list_at;
-
-    return reserve_fn == 0 || capacity_fn == 0 || at_fn == 0;
-}
