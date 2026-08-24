d5dff0e82fc5664bca93f4b8458b168aa12dcb2b
Qi Gao <qi.gao2012@gmail.com>
2026-08-24 17:44:50 +0800

    formal(cflow): prove readiness slot safety

--- formal/cmeta_cflow_calculus/Test/PhaseATests.lean
+++ formal/cmeta_cflow_calculus/Test/PhaseATests.lean
@@ -14,6 +14,7 @@ import PhaseATests.Mailbox
 import PhaseATests.MachineSchema
 import PhaseATests.Machine
 import PhaseATests.MachineRuntime
+import PhaseATests.Readiness
 
 open CMetaCFlowCalculus.CMeta
 open CMetaCFlowCalculus.CFlow
