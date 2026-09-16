7b2fa43bc243e90e784cea75bea4da0076eaef65
Qi Gao <qi.gao2012@gmail.com>
2026-09-16 08:50:01 +0800

    test(cnet): scope A/A control to IOCP

--- cnet/tests/cnet_io_benchmark_config_test.c
+++ cnet/tests/cnet_io_benchmark_config_test.c
@@ -58,10 +58,15 @@ spec("CNet I/O benchmark backend selection") {
 #endif
   }
 
-  it("requires an identical-path NativeIO direct A/A null control") {
+  it("enables the identical-path NativeIO direct A/A control only on IOCP") {
     cnet_io_benchmark_protocol protocol = {0};
 
     check_equal(cnet_io_benchmark_protocol_default(&protocol), SALTS_OK);
+#ifdef _WIN32
     check_true(protocol.native_direct_aa_control);
+#else
+    check_false(protocol.native_direct_aa_control);
+#endif
+    check_equal(cnet_io_benchmark_protocol_default(NULL), SALTS_EINVAL);
   }
 }
