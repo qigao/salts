9bc515a3e6aebde1e6c8cb5837607f2b47445733
Qi Gao <qi.gao2012@gmail.com>
2026-09-16 08:49:39 +0800

    perf(cnet): define benchmark null-control protocol

--- cnet/benchmarks/cnet_io_benchmark_config.h
+++ cnet/benchmarks/cnet_io_benchmark_config.h
@@ -3,11 +3,18 @@
 
 #include <salts/native_io.h>
 
+#include <stdbool.h>
+
 typedef struct cnet_io_benchmark_backend {
   native_io_backend_kind kind;
   const char *name;
 } cnet_io_benchmark_backend;
 
+typedef struct cnet_io_benchmark_protocol {
+  bool native_direct_aa_control;
+} cnet_io_benchmark_protocol;
+
 int cnet_io_benchmark_select_backend(const char *requested, cnet_io_benchmark_backend *selected);
+int cnet_io_benchmark_protocol_default(cnet_io_benchmark_protocol *protocol);
 
 #endif
