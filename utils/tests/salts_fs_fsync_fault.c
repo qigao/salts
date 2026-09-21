c1feadf93a1cc4842efd7b1c55230be6a16aea13
Qi Gao <qi.gao2012@gmail.com>
2026-09-21 20:08:29 +0800

    test(fs): parameterize durable replace fsync faults

--- utils/tests/salts_fs_fsync_fault.c
+++ utils/tests/salts_fs_fsync_fault.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <errno.h>
 #include <stdatomic.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
@@ -8,7 +9,9 @@ static _Atomic int salts_test_fsync_calls = 0;
 
 int fsync(int fd) {
   const int call = atomic_fetch_add(&salts_test_fsync_calls, 1) + 1;
-  if (call == 2) {
+  const char *requested = getenv("SALTS_FS_TEST_FAIL_FSYNC_CALL");
+  const int fail_call = requested != NULL ? atoi(requested) : 2;
+  if (call == fail_call) {
     errno = EIO;
     return -1;
   }
