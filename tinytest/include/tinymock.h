29cc102ff20dcf4490e484c451f721ed78743f90
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 10:54:05 +0800

    refactor(tinymock): route generated mocks through dispatch

--- tinytest/include/tinymock.h
+++ tinytest/include/tinymock.h
@@ -177,6 +177,14 @@ void tinymock_mock_script_error(tinymock_mock_t *mock, const char *message);
 tinymock_value_t tinymock_mock_dispatch(tinymock_mock_t *mock, size_t argc,
                                         const tinymock_value_t *actual_args);
 
+size_t tinymock_mock_call_count(const tinymock_mock_t *mock);
+const tinymock_recorded_call_t *tinymock_mock_call_at(
+    const tinymock_mock_t *mock, size_t index);
+void tinymock_mock_verify_times(tinymock_mock_t *mock, size_t expected);
+void tinymock_mock_verify_never(tinymock_mock_t *mock);
+void tinymock_mock_verify_at_least(tinymock_mock_t *mock, size_t minimum);
+void tinymock_mock_verify_at_most(tinymock_mock_t *mock, size_t maximum);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
@@ -270,7 +278,7 @@ static inline void tinymock_failure_adapter__(const char *message) {
   TINYMOCk_GENERATED__ RET NAME(TINYMOCk_PARAMS__(__VA_ARGS__)) { \
     tinymock_value_t actual_args[] = {TINYMOCk_ACTUAL_VALUES__(__VA_ARGS__)}; \
     tinymock_value_t result = \
-        tinymock_mock_invoke(&TINYMOCk_MOCK_DATA(NAME), (size_t)(count), actual_args); \
+        tinymock_mock_dispatch(&TINYMOCk_MOCK_DATA(NAME), (size_t)(count), actual_args); \
     return TINYMOCk_VALUE_AS(RET, result); \
   }
 
@@ -286,7 +294,7 @@ static inline void tinymock_failure_adapter__(const char *message) {
   } \
   TINYMOCk_GENERATED__ void NAME(TINYMOCk_PARAMS__(__VA_ARGS__)) { \
     tinymock_value_t actual_args[] = {TINYMOCk_ACTUAL_VALUES__(__VA_ARGS__)}; \
-    (void)tinymock_mock_invoke(&TINYMOCk_MOCK_DATA(NAME), (size_t)(count), actual_args); \
+    (void)tinymock_mock_dispatch(&TINYMOCk_MOCK_DATA(NAME), (size_t)(count), actual_args); \
   }
 
 #define TINYMOCk_MOCK_SELECT__(RET, NAME, count, ...) \
@@ -308,7 +316,7 @@ static inline void tinymock_failure_adapter__(const char *message) {
     tinymock_mock_expect(&TINYMOCk_MOCK_DATA(NAME), 0, NULL, true, ret); \
   } \
   TINYMOCk_GENERATED__ RET NAME(void) { \
-    tinymock_value_t result = tinymock_mock_invoke(&TINYMOCk_MOCK_DATA(NAME), 0, NULL); \
+    tinymock_value_t result = tinymock_mock_dispatch(&TINYMOCk_MOCK_DATA(NAME), 0, NULL); \
     return TINYMOCk_VALUE_AS(RET, result); \
   }
 
@@ -318,7 +326,7 @@ static inline void tinymock_failure_adapter__(const char *message) {
     tinymock_mock_expect(&TINYMOCk_MOCK_DATA(NAME), 0, NULL, false, tinymock_value_zero()); \
   } \
   TINYMOCk_GENERATED__ void NAME(void) { \
-    (void)tinymock_mock_invoke(&TINYMOCk_MOCK_DATA(NAME), 0, NULL); \
+    (void)tinymock_mock_dispatch(&TINYMOCk_MOCK_DATA(NAME), 0, NULL); \
   }
 
 #define TINYMOCk_MOCK_DECLARE(NAME) extern tinymock_mock_t TINYMOCk_MOCK_DATA(NAME)
