b7d863d5ff1ef32d91ac7c5e0215b52dd62269ec
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 15:40:23 +0800

    test(tinymock): call installed void replacement

--- tests/tinymock_installed_consumer/consumer.c
+++ tests/tinymock_installed_consumer/consumer.c
@@ -3,3 +3,7 @@
 int tinymock_installed_consumer_run(int value) {
   return tinymock_installed_add(value, 2);
 }
+
+void tinymock_installed_consumer_shutdown(void) {
+  tinymock_installed_shutdown();
+}
