3ace81ea45acd55face0532c3307fedb037018a8
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 22:01:01 +0800

    test(tinymock): add second selective fixture header

---
+++ tinymock/test/tinymock_selective_extra_fixture.h
@@ -0,0 +1,9 @@
+#ifndef TINYMOCK_SELECTIVE_EXTRA_FIXTURE_H
+#define TINYMOCK_SELECTIVE_EXTRA_FIXTURE_H
+
+#include <cmeta/function.h>
+
+FunctionDecl(value, int, tinymock_selective_extra,
+    (int, value, CMETA_PARAM_IN));
+
+#endif /* TINYMOCK_SELECTIVE_EXTRA_FIXTURE_H */
