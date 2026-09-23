8efd6c42cb36e2dccca67ac722ad91f6c7f8146f
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 21:58:43 +0800

    test(tinymock): add static inline admission fixture

---
+++ tinymock/test/compile_fail/tinymock_static_inline_header.h
@@ -0,0 +1,8 @@
+#ifndef TINYMOCK_STATIC_INLINE_HEADER_H
+#define TINYMOCK_STATIC_INLINE_HEADER_H
+
+static inline int tinymock_bad_inline(int value) {
+  return value;
+}
+
+#endif /* TINYMOCK_STATIC_INLINE_HEADER_H */
