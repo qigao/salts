beb0a22196d7ca9e41f37a3633c3e88482a1fa45
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 18:19:30 +0800

    test(tinymock): declare aggregate consumers

--- tinymock/test/tinymock_function_consumer.h
+++ tinymock/test/tinymock_function_consumer.h
@@ -2,6 +2,7 @@
 #define TINYMOCK_FUNCTION_CONSUMER_H
 
 #include <stddef.h>
+#include "tinymock_function_fixture.h"
 
 int tinymock_function_consumer_run(int input);
 int tinymock_function_consumer_pointer(int *value);
@@ -11,5 +12,7 @@ int tinymock_function_consumer_unknown_ptr(int *value);
 int tinymock_function_consumer_nullable_out(size_t *written);
 void tinymock_function_consumer_notify(int event, size_t *written);
 void tinymock_function_consumer_shutdown(void);
+tinymock_fixture_box tinymock_function_consumer_box(tinymock_fixture_box input);
+int *tinymock_function_consumer_pointer_answer(void);
 
 #endif /* TINYMOCK_FUNCTION_CONSUMER_H */
