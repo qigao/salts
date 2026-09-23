3e4538870e9294852539319b8476c1f55bc4a679
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 15:09:18 +0800

    test(tinymock): declare output-parameter consumers

--- tinymock/test/tinymock_function_consumer.h
+++ tinymock/test/tinymock_function_consumer.h
@@ -3,5 +3,9 @@
 
 int tinymock_function_consumer_run(int input);
 int tinymock_function_consumer_pointer(int *value);
+int tinymock_function_consumer_write_size(int input, size_t *written);
+int tinymock_function_consumer_adjust_int(int *value);
+int tinymock_function_consumer_unknown_ptr(int *value);
+int tinymock_function_consumer_nullable_out(size_t *written);
 
 #endif /* TINYMOCK_FUNCTION_CONSUMER_H */
