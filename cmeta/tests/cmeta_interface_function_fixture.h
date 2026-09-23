85a2c4586420df5b220cc78912cfaac0f6a9b4c0
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 00:27:08 +0800

    test(cmeta): add fully reflected owning interface fixture

--- cmeta/tests/cmeta_interface_function_fixture.h
+++ cmeta/tests/cmeta_interface_function_fixture.h
@@ -14,4 +14,10 @@
 
 CMETA_INTERFACE(cmeta_reflected_counter, CMETA_REFLECTED_COUNTER_METHODS);
 
+#define CMETA_REFLECTED_OWNER_METHODS(X,I) \
+    X(I,FD0,void,destroy,stateful, \
+      &cmeta_type_void,CMETA_ABI_VOID)
+
+CMETA_INTERFACE(cmeta_reflected_owner, CMETA_REFLECTED_OWNER_METHODS);
+
 #endif /* CMETA_INTERFACE_FUNCTION_FIXTURE_H */
