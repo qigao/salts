f5c066317a9c7792c5c75d62df999488bb81c8a3
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 23:54:09 +0800

    test(cmeta): add fully reflected interface fixture

---
+++ cmeta/tests/cmeta_interface_function_fixture.h
@@ -0,0 +1,17 @@
+#ifndef CMETA_INTERFACE_FUNCTION_FIXTURE_H
+#define CMETA_INTERFACE_FUNCTION_FIXTURE_H
+
+#include <cmeta/interface.h>
+
+#define CMETA_REFLECTED_COUNTER_METHODS(X,I) \
+    X(I,F1,int,add,value, \
+      &cmeta_type_int,CMETA_ABI_SCALAR, \
+      (int,delta,CMETA_PARAM_IN,&cmeta_type_int,CMETA_ABI_SCALAR)) \
+    X(I,F0,int,value,value, \
+      &cmeta_type_int,CMETA_ABI_SCALAR) \
+    X(I,FV0,void,reset,stateful, \
+      &cmeta_type_void,CMETA_ABI_VOID)
+
+CMETA_INTERFACE(cmeta_reflected_counter, CMETA_REFLECTED_COUNTER_METHODS);
+
+#endif /* CMETA_INTERFACE_FUNCTION_FIXTURE_H */
