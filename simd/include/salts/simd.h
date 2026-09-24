2c6d778084861cf1d690bb7bf42756ef1b3ed0b5
Qi Gao <qi.gao2012@gmail.com>
2026-09-24 09:23:15 +0800

    feat(simd): declare descriptor-driven numeric conversions

--- simd/include/salts/simd.h
+++ simd/include/salts/simd.h
@@ -86,6 +86,19 @@ typedef enum salts_simd_half {
     SALTS_SIMD_HALF_HIGH
 } salts_simd_half;
 
+typedef enum salts_simd_convert_op {
+    SALTS_SIMD_CONVERT_TRUNC_SAT = 0,
+    SALTS_SIMD_CONVERT_NUMERIC,
+    SALTS_SIMD_CONVERT_DEMOTE,
+    SALTS_SIMD_CONVERT_PROMOTE
+} salts_simd_convert_op;
+
+typedef enum salts_simd_lane_policy {
+    SALTS_SIMD_LANES_FULL = 0,
+    SALTS_SIMD_LANES_LOW,
+    SALTS_SIMD_LANES_LOW_ZERO
+} salts_simd_lane_policy;
+
 void salts_simd_v128_load(salts_v128 *out, const void *source);
 void salts_simd_v128_store(void *destination, const salts_v128 *value);
 
