eb25aad4b6649f2b5a853f467d332df547b1d307
Qi Gao <qi.gao2012@gmail.com>
2026-09-25 13:13:52 +0800

    refactor(cmeta): remove duplicate Salts fixed-width integer authority

--- utils/include/salts_cmeta_fixed_width.h
+++ utils/include/salts_cmeta_fixed_width.h
@@ -77,28 +77,11 @@ static const cmeta_data_desc salts_bool8_cmeta_data = {
     &salts_bool8_cmeta_fixed_ops
 };
 
-#define SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(name_, c_type_, data_kind_, bits_, id_)             \
-  static const cmeta_type_identity name_##_cmeta_identity = CMETA_TYPE_ID_ATOM_INIT(id_);          \
-  static const cmeta_type_desc name_##_cmeta_type = {                                              \
-      #c_type_, sizeof(c_type_),        CMETA_ALIGNOF(c_type_), CMETA_T_INTEGER, NULL,             \
-      NULL,     &name_##_cmeta_identity};                                                          \
-  static const cmeta_data_integer_shape name_##_cmeta_shape = {bits_};                             \
-  static const cmeta_data_desc name_##_cmeta_data = {                                              \
-      sizeof(cmeta_data_desc), CMETA_DATA_DESC_ABI_VERSION, id_ ".data", #c_type_, data_kind_,     \
-      &name_##_cmeta_type,     &name_##_cmeta_shape,        NULL,        NULL,     NULL, NULL}
+/*
+ * Exact-width integer authority lives in CMeta core. Keep no Salts-private
+ * duplicate descriptors here: consumers use cmeta_type_int32 /
+ * cmeta_data_int32 (and the corresponding widths/signs) directly.
+ */
 
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_int8, int8_t, CMETA_DATA_SINT, 8u, "salts.int8");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_int16, int16_t, CMETA_DATA_SINT, 16u, "salts.int16");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_int32, int32_t, CMETA_DATA_SINT, 32u, "salts.int32");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_int64, int64_t, CMETA_DATA_SINT, 64u, "salts.int64");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_uint8, uint8_t, CMETA_DATA_UINT, 8u, "salts.uint8");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_uint16, uint16_t, CMETA_DATA_UINT, 16u,
-                                       "salts.uint16");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_uint32, uint32_t, CMETA_DATA_UINT, 32u,
-                                       "salts.uint32");
-SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER(salts_uint64, uint64_t, CMETA_DATA_UINT, 64u,
-                                       "salts.uint64");
-
-#undef SALTS_CMETA_DEFINE_FIXED_WIDTH_INTEGER
 
 #endif /* SALTS_CMETA_FIXED_WIDTH_H */
