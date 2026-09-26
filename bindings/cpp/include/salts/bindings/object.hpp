c3744cd526694ab4c564bf0cb0817d18572ce055
Qi Gao <qi.gao2012@gmail.com>
2026-09-27 01:22:42 +0800

    feat(bindings): centralize C++ object admission

--- bindings/cpp/include/salts/bindings/object.hpp
+++ bindings/cpp/include/salts/bindings/object.hpp
@@ -3,6 +3,8 @@
 
 #include <cmeta/object.h>
 
+#include <type_traits>
+
 namespace Salts {
 
 template <typename T>
@@ -12,9 +14,32 @@ struct Borrowed {
 
 template <typename T>
 constexpr Borrowed<T> borrow(T &object) noexcept {
+  static_assert(!std::is_const_v<T>,
+                "Salts::borrow requires mutable native storage");
   return Borrowed<T>{&object};
 }
 
+namespace detail {
+
+template <typename T>
+inline cmeta_status bind_object_ref(
+    cmeta_object_ref *out, Borrowed<T> borrowed,
+    const cmeta_data_desc *data,
+    const cmeta_object_field_provider *field_provider,
+    const cmeta_object_method_provider *method_provider) noexcept {
+  static_assert(!std::is_const_v<T>,
+                "CMeta object references require mutable native storage");
+  if (out == nullptr || borrowed.object == nullptr || data == nullptr)
+    return CMETA_INVALID_ARGUMENT;
+  if (field_provider != nullptr || method_provider != nullptr)
+    return cmeta_object_borrow_with_providers(
+        out, static_cast<void *>(borrowed.object), data,
+        field_provider, method_provider);
+  return cmeta_object_borrow(
+      out, static_cast<void *>(borrowed.object), data, nullptr);
+}
+
+}  // namespace detail
 }  // namespace Salts
 
 #endif
