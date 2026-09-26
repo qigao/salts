d0cd071c979df6d45ecbbd48bcc647389baded3f
Qi Gao <qi.gao2012@gmail.com>
2026-09-26 12:14:29 +0800

    test(cmeta): expose receiver resolution to C++17

--- cmeta/tests/cmeta_header_cpp_test.cpp
+++ cmeta/tests/cmeta_header_cpp_test.cpp
@@ -23,6 +23,8 @@ static_assert(std::is_standard_layout_v<cmeta_receiver_method>,
               "receiver method entries remain a C ABI");
 static_assert(std::is_standard_layout_v<cmeta_receiver_method_set>,
               "receiver method sets remain a C ABI");
+static_assert(std::is_standard_layout_v<cmeta_receiver_resolution>,
+              "receiver call resolutions remain a C ABI");
 static_assert(std::is_standard_layout_v<cmeta_interface_method_desc>,
               "interface method reflection remains a C ABI");
 static_assert(std::is_standard_layout_v<cmeta_interface_desc>,
