13bbd4416b0c0d031d623b0544c706bc3fe88e0e
Qi Gao <qi.gao2012@gmail.com>
2026-09-26 19:58:37 +0800

    feat(cnet): make write capacity policy explicit

--- cnet/src/cnet_write_queue.h
+++ cnet/src/cnet_write_queue.h
@@ -16,6 +16,7 @@ typedef struct cnet_write_queue {
 typedef struct cnet_write_queue_config {
   size_t connection_capacity;
   size_t capacity;
+  size_t per_connection_capacity;
   size_t max_payload_bytes;
   /** Aggregate live copied bytes. Retained buffers do not consume this budget. */
   size_t payload_capacity_bytes;
@@ -44,6 +45,7 @@ typedef struct cnet_write_queue_stats {
   size_t peak_copied_bytes;
   size_t capacity;
   size_t connection_capacity;
+  size_t per_connection_capacity;
   bool admission_open;
 } cnet_write_queue_stats;
 
